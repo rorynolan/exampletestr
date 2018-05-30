a05031e4635794604e602694a8da1a028dc7feff
Rory Nolan <rorynoolan@gmail.com>
2018-05-30 14:36:57 +0100

    Help GitHub recognise repo as R package.

--- junk/sticker.R
+++ junk/sticker.R
@@ -1,12 +1,12 @@
 pkgs <- c("hexSticker", "tidyverse", "here", "magick")
 invisible(lapply(pkgs, library, character.only = TRUE))
 
-sticker(here("junk", "ijtiff.png"),
-        package = "ijtiff",
+sticker(here("junk", "exampletestr.png"),
+        package = "exampletestr",
         filename = here("junk", "sticker.png"),
-        s_width = 0.7,
-        s_x = 0.99, s_y = 0.8,
-        p_y = 1.63,
-        url = "github.com/ropensci/ijtiff",
+        s_width = 0.48,
+        s_x = 1.035, s_y = 0.86,
+        p_y = 1.6, p_size = 5,
+        url = "github.com/rorynolan/exampletestr",
         u_x = 1.13, u_color = "white", u_size = 0.8)
 image_read(here("junk", "sticker.png"))
